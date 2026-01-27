75c273fa10c54ff49d070ece8cc59b0eb85c3a56
Rei Schädeli <rei@fae.red>
2026-01-27 09:31:49 +0100

    fix: scss on subpath

--- server/src/index.ts
+++ server/src/index.ts
@@ -113,7 +113,7 @@ io.use(wrap(session));
 
 app.use(async (req, res, next) => {
   if (req.url.endsWith(".scss")) {
-    const filename = `${__dirname}/../public/${req.path.replace("/res/", "")}`;
+    const filename = `${__dirname}/../public/${req.path.replace(/.*\/res\//, "")}`;
     if (!fs.existsSync(filename)) {
       Logger.warn(`scss file ${filename} not found`)
       next()
